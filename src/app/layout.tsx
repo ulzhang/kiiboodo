a3cb0edb66257ba5e85772b5f1b6ad9ea0e91067
Alexander Chen <alexander.qili.chen@gmail.com>
2024-02-01 18:37:52 +0800

    Removed the device checking bc it doesnt work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import Script from "next/script";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { isMobile, isTablet } from "react-device-detect";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,15 +31,7 @@ export default function RootLayout({
           gtag('config', 'G-3XWS80C1HX');
         `}
       </Script>
-      <body className={inter.className}>
-        {children}
-        {isMobile ||
-          (isTablet && (
-            <p className="fixed w-full bg-blue-900 bottom-0 text-center py-2 text-lg">
-              Sorry, this doesn&apos;t really work on mobile yet 😿
-            </p>
-          ))}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
