b9e862b2f3f609e132c0854cb7a5c1c8e7d130af
Alexander Chen <alexander.qili.chen@gmail.com>
2024-02-01 17:40:32 +0800

    Add type to children

--- src/components/KeyRow.tsx
+++ src/components/KeyRow.tsx
@@ -1,4 +1,6 @@
-const KeyRow = ({ children }) => {
+import { ReactNode } from "react";
+
+const KeyRow = ({ children }: { children: ReactNode }) => {
   return <div className="flex gap-2">{children}</div>;
 };
 export default KeyRow;
