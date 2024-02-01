dcaa11af34a337e1ef74a4f96d96b509502996f0
Alexander Chen <alexander.qili.chen@gmail.com>
2024-02-01 17:49:21 +0800

    Add gogle analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -16,6 +17,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-3XWS80C1HX"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-3XWS80C1HX');
+        `}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   );
